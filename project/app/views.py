683d30caf0b35417b5efb79acf4386f38b590eb6
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-08-30 11:11:57 +0530

    weather

--- project/app/views.py
+++ project/app/views.py
@@ -11,9 +11,9 @@ def index(request):
 
         city_name='Kochi'
 
-        url=f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid=360e4bc3865e745ec844bd7ec054ca11
+        url=f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid=360e4bc3865e745ec844bd7ec054ca11'
 
-        data-requests.get(url)
+        data=requests.get(url)
 
         weather_data=data.json()
 
@@ -23,36 +23,36 @@ def index(request):
 
         data={
 
-        'city:city_name,
+            'city':city_name,
 
-        'weather_discription':weather_data['weather'][0]['description'],
+            'weather_discription':weather_data['weather'][0]['description'],
 
-        'temperature_kelvin' :weather_data['main']['temp'],
+            'temperature_kelvin':weather_data['main']['temp'],
 
-        'temperature_celsius':math.floor(weather_data['main']['temp']- 273.15), #Convert to Celsius
+            'temperature_celsius':math.floor(weather_data['main']['temp']- 273.15), #Convert to Celsius
 
-        'humidity':weather_data['main'] ['humidity'],
+            'humidity':weather_data['main']['humidity'],
 
-        'temp_min_celsius':math.floor(weather_data['main']['temp_min'] 273.15), #Convert to Celsius
+            'temp_min_celsius':math.floor(weather_data['main']['temp_min']-273.15), #Convert to Celsius
 
-        'temp_max_celsius':math.floor(weather_data['main']['temp_max'] 273.15), #Convert to Celsius
+            'temp_max_celsius':math.floor(weather_data['main']['temp_max']-273.15), #Convert to Celsius
 
-        'sunrise':dt_object.strftime('%H:%M:%S'), sunset: dt_object1.strftime('%H:%M:%S'),
+            'sunrise':dt_object.strftime('%H:%M:%S'),'sunset': dt_object1.strftime('%H:%M:%S'),
 
-        'visibility':math.floor(weather_data['visibility']/1000),
+            'visibility':math.floor(weather_data['visibility']/1000),
 
-        'cloud':weather_data['clouds']['all'],
+            'cloud':weather_data['clouds']['all'],
 
-        "wind":weather_data['wind'] ['speed'],
+            'wind':weather_data['wind']['speed'],
 
-        'pressure':math.floor((weather_data['main']['pressure']/1013.25)*100),
+            'pressure':math.floor((weather_data['main']['pressure']/1013.25)*100),
 
-        'feel_temp':math.floor(weather_data['main']['feels_like']-273.15),
+            'feel_temp':math.floor(weather_data['main']['feels_like']-273.15),
 
-        'sea_level':weather_data['main']['sea_level'], 
-        
-        'grnd_level':weather_data['main']['grnd_level'],
+            'sea_level':weather_data['main']['sea_level'], 
+            
+            'grnd_level':weather_data['main']['grnd_level'],
 
-}
+    }
 
-return render(request, 'index.html',('data':data))
+    return render (request,'index.html',{'data':data})
